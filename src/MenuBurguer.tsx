f7d81578dbfde0d48eb300027790ca4f86e38236
Caio Lima EK <caio.lima@elringklinger.com>
2024-07-25 16:19:42 -0300

    modified:   src/MenuBurguer.tsx modified:   src/components/SidebarMenuBurguer.tsx

--- src/MenuBurguer.tsx
+++ src/MenuBurguer.tsx
@@ -1,20 +1,9 @@
-import { useState } from "react"
-import SidebarMenuBurguer from "./components/SidebarMenuBurguer"
 import { Link } from "react-router-dom"
 
 function MenuBurguer() {
 
-  const [open, setOpen] = useState<boolean>(false)
-
   return (
-        <aside>
-          <SidebarMenuBurguer open={open} onClose={() => setOpen(false)}>
-            <div className="">
-              <h1 className="text-black">Modal Title</h1>
-              <p>Qualquer coisa</p>
-            </div>
-          </SidebarMenuBurguer>
-          
+        <aside>         
             <button>
             <Link to={"/NewModal"} className="">
               New Modal
--- src/components/SidebarMenuBurguer.tsx
+++ src/components/SidebarMenuBurguer.tsx
@@ -1,15 +1,9 @@
-import { MouseEventHandler, ReactNode } from "react"
 
-function SidebarMenuBurguer({open, onClose, children} : {open: boolean; onClose: MouseEventHandler; children: ReactNode }) {
+
+function SidebarMenuBurguer(){
     return (
-        <div className={`${open ? "" : ""}`} onClick={onClose}>
-        <div className={`${open ? "" : ""}`} onClick={(e) => e.stopPropagation()}>
-            <button className="" onClick={onClose}>
-              
-            </button>
-            {children}
-        </div>
-      </div>
+        <>
+        </>
     )
 }
 export default SidebarMenuBurguer
