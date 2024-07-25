05f4231a604e2adab3f5b86016aacb3a19b78ccf
Caio Lima EK <caio.lima@elringklinger.com>
2024-07-25 10:15:07 -0300

    C

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
       <div className="p-10 flex justify-center w-full">
-        <button className="border border-neutral-300 rounded-lg py=1.5 px-10 my-2 bg-blue500 hover:bg-blue-600 text-white" onClick={() => setOpen(true)}>
+        <button className="border border-neutral-300 rounded-lg py=1.5 px-10 my-2 bg-blue-500 hover:bg-blue-600 text-white" onClick={() => setOpen(true)}>
             Open
         </button>
 
@@ -17,7 +17,12 @@ function App() {
             <p>Qualquer coisa</p>
             <hr className="border-t-solid border-1 border-gray"/>
             <div className="flex flex-row justify-center">
-              <button className="border border-neutral-300 rounded-lg py-1.5 px-10 bg-blue-500 hover:bg-blue-500 text-white" onClick={() => setOpen(false)}></button>
+              <button className="border border-neutral-300 rounded-lg py-1.5 px-10 bg-blue-500 hover:bg-blue-500 text-white" onClick={() => setOpen(false)}>
+                Close
+              </button>
+              <button className="border border-neutral-300 rounded-lg py-1.5 px-10 bg-blue-500 hover:bg-blue-500 text-white" onClick={() => setOpen(false)}>
+                Close 2
+              </button>
             </div>
           </div>
         </Modal>
