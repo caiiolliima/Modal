8f78bddfb9fa5be913a0f6969055a7350c7d5b25
Caio Lima <lima.caioalves@gmail.com>
2024-07-25 05:57:41 -0300

    modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        
+      <div className="p-10 flex justify-center w-full">
+        <button className="border border-neutral-300 rounded-lg py=1.5 px-10 my-2 bg-blue500 hover:bg-blue-600 text-white">
+            Open
+        </button>
       </div>
     </>
   )
